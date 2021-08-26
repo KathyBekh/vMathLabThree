fc7319230c6f761eb7d99b2e8b930d461daf5710
Katerina Behtold <kbehtold@yandex.ru>
2021-08-26 16:16:15 +0300

    Исправила вывод на экран. Рисуются графики.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from math import exp
 import numpy
-from matplotlib import pyplot
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
 
 import Adams2
 import Runge_Kutta3
@@ -34,52 +36,43 @@ def originalFunction(t, y):
 
 # вывод результатов на экран
 def printResult():
-    pyplot.title('Зеленый - ; красный - rkf45, черный - rk4, желтый - rk3, ...')
+    plt.title('Зеленый - exact; красный - rkf45, черный - rk4, желтый - rk3, ...')
 
     t, y_exact = ExactSolution.pick_step(a, b + h, step=h)
-    pyplot.plot(t, y_exact, 'g--')
+    plt.plot(t, y_exact, 'g--')
 
     y_rkf45 = rkf45.rkf45(originalFunction, t, initialConditions)
-    pyplot.plot(t, y_rkf45, 'r--')
+    plt.plot(t, y_rkf45, 'r--')
 
     y_rk4 = Runge_Kutta4.rk4Values(originalFunction, t, initialConditions)
-    pyplot.plot(t, y_rk4, 'k')
+    plt.plot(t, y_rk4, 'k')
 
     y_rk3 = Runge_Kutta3.rk3Values(originalFunction, t, initialConditions)
-    pyplot.plot(t, y_rk3, 'y')
+    plt.plot(t, y_rk3, 'y')
 
     y_adams = Adams2.adams2(originalFunction, t, initialConditions)
-    pyplot.plot(t, y_adams, 'm')
-    #
-    # pyplot.show()
+    plt.plot(t, y_adams, 'm')
 
-    errer_local_rkf45 = numpy.abs(y_rkf45 - y_exact)
-    errer_local_rk4 = numpy.abs(y_rk4 - y_exact)
-    errer_local_rk3 = numpy.abs(y_rk3 - y_exact)
-    errer_local_adams = numpy.abs(y_adams - y_exact)
+    plt.show()
 
-    print('First step of RKF45: ', errer_local_rkf45[1])
-    print('First step of RK4: ', errer_local_rk4[1])
-    print('First step of RK3: ', errer_local_rk3[1])
-    print('First step of ADAMS: ', errer_local_adams[1])
+    error_local_rkf45 = numpy.abs(y_rkf45 - y_exact)
+    error_local_rk4 = numpy.abs(y_rk4 - y_exact)
+    error_local_rk3 = numpy.abs(y_rk3 - y_exact)
+    error_local_adams = numpy.abs(y_adams - y_exact)
 
-    print('Global of RKF45: ', errer_local_rkf45.sum())
-    print('Global of RK4: ', errer_local_rk4.sum())
-    print('Global of RK3: ', errer_local_rk3.sum())
-    print('Global of ADAMS: ', errer_local_adams.sum())
-
-    # print('h^5 is about: ', h ** 5)
-    # print('h^4 is about: ', h ** 4)
-    # print('h^3 is about: ', h ** 3)
-    # print('h^5 / RKF45 first step: ', h ** 5 / errer_local_rkf45[1])
-    # # print('h^3 / ADAMS first step: ', h ** 3 / errer_local_adams[1])
-
-    print('\t\t\t\tValues')
-    print('t\t\t\t\tExact\t\t\t\tRKF45\t\t\t\tRK4\t\t\t\tRK3\t\t\t\tADAMS')
+    print('\tTable of Values: ')
+    print('t\t\t\t\tExact\t\t\t\tRKF45\t\t\t\tRK4\t\t\t\t\tRK3\t\t\t\tADAMS')
 
     for it in range(0, len(t)):
         print('{:0.2f} \t {}\t {}\t {}\t {}\t {}'.format(t[it],
                                                          y_exact[it], y_rkf45[it], y_rk4[it], y_rk3[it], y_adams[it]))
+
+    print('\tTable of Errors: ')
+    print('\t\t\t\t\tRKF45\t\t\t\t\tRK4\t\t\t\t\t\tRK3\t\t\t\t\tADAMS')
+    print('Local Error: \t {}\t {}\t {}\t {}'.format(error_local_rkf45[1], error_local_rk4[1], error_local_rk3[1],
+                                                     error_local_adams[1]))
+    print('Global Error: \t {}\t {}\t {}\t\t {}'.format(error_local_rkf45.sum(), error_local_rk4.sum(),
+                                                        error_local_rk3.sum(), error_local_adams.sum()))
 
 
 if __name__ == '__main__':
